c6b1bf74805f770ea24a1e0874a816ad5a1ca4e4
dmitry <dmitrijlukanin8@gmail.com>
2020-12-21 05:17:50 +0500

    add products on main page

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<slug:category_slag>', views.home, name='products_by_category'),
     path('product/', views.product, name='product'),
+
 ]

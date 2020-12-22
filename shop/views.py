712d2a117e48c1cb6e8c7703b260722c9450ad2c
dmitry <dmitrijlukanin8@gmail.com>
2020-12-22 08:05:15 +0500

    Revert "create template for cart page"
    This reverts commit 9a8e3cc4

--- shop/views.py
+++ shop/views.py
@@ -22,6 +22,3 @@ def product(request, category_slug, product_slug):
     except Exception as e:
         raise e
     return render(request, 'product.html', {'product': product})
-
-def cart(request):
-    return render(request, 'cart.html')

b6f62987be2d31ad6c57b9533323bf807751ca4e
dmitry <dmitrijlukanin8@gmail.com>
2020-12-22 05:23:22 +0500

    display all information about product from DB

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+
+
 # Create your views here.
 
 def home(request, category_slug=None):
@@ -7,11 +9,16 @@ def home(request, category_slug=None):
     products = None
     if category_slug != None:
         category_page = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category = category_page, available=True)
+        products = Product.objects.filter(category=category_page, available=True)
     else:
         products = Product.objects.all().filter(available=True)
 
     return render(request, 'home.html', {'category': category_page, 'products': products})
 
-def product(request):
-    return render(request, 'product.html')
+
+def product(request, category_slug, product_slug):
+    try:
+        product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    return render(request, 'product.html', {'product': product})
